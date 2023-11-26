2894e92b35ab57d361b57a539947820256f962b4
ThemySabri <themysabriii@gmail.com>
2023-11-26 19:17:21 +0700

    first commit to this github acc

--- Kodingan Kelas/8 - Pohon Biner/machine.c
+++ Kodingan Kelas/8 - Pohon Biner/machine.c
@@ -2,85 +2,82 @@
 
 void makeTree(char c, tree *T)
 {
-    simpul *baru;
-    baru = (simpul *)malloc(sizeof(simpul));
-    baru->kontainer = c;
-    baru->right = NULL;
-    baru->left = NULL;
-    (*T).root = baru;
-    baru = NULL;
+    simpul *baru;                            // bikin simpul baru
+    baru = (simpul *)malloc(sizeof(simpul)); // alokasi simpul baru
+    baru->kontainer = c;                     // isi simpul dnegan char c
+    baru->right = NULL;                      // null-kan anak kanan
+    baru->left = NULL;                       // null-kan anak kiri
+    (*T).root = baru;                        // jadikan si baru itu jadi root dari pohon T
+    baru = NULL;                             // barunya jadiin null
 }
 
 void addLeft(char c, simpul *root)
 {
-    if (root->left == NULL)
+    if (root->left == NULL) // jika sub pohon kiri kosong
     {
-        /*jika sub pohon kiri kosong*/
-        simpul *baru;
-        baru = (simpul *)malloc(sizeof(simpul));
-        baru->kontainer = c;
-        baru->right = NULL;
-        baru->left = NULL;
-        root->left = baru;
+        simpul *baru;                            // bikin simpul baru
+        baru = (simpul *)malloc(sizeof(simpul)); // alokasi simpul
+        baru->kontainer = c;                     // isi si simpul tersebut dengan char c
+        baru->right = NULL;                      // null-kan anak kanan
+        baru->left = NULL;                       // null kan anak kiri
+        root->left = baru;                       // jadikan si simpul tsb. sebagai anak kiri dari root (parents)
     }
     else
     {
-        printf("sub pohon kiri telah terisi \n");
+        printf("sub pohon kiri telah terisi \n"); // kasih error message kalo emang udah ada isinya
     }
 }
 
 void addRight(char c, simpul *root)
 {
-    if (root->right == NULL)
+    if (root->right == NULL) // jika sub pohon kanan kosong
     {
         /*jika sub pohon kanan kosong*/
-        simpul *baru;
-        baru = (simpul *)malloc(sizeof(simpul));
-        baru->kontainer = c;
-        baru->right = NULL;
-        baru->left = NULL;
-        root->right = baru;
+        simpul *baru;                            // bikin simpul baru
+        baru = (simpul *)malloc(sizeof(simpul)); // alokasi simpul
+        baru->kontainer = c;                     // isi si simpul tersebut dengan char c
+        baru->right = NULL;                      // null-kan anak kanan
+        baru->left = NULL;                       // null kan anak kiri
+        root->right = baru;                      // jadikan si simpul tsb. sebagai anak kiri dari root (parents)
     }
     else
     {
-        printf("sub pohon kanan telah terisi \n");
+        printf("sub pohon kanan telah terisi \n"); // kasih error message kalo emang udah ada isinya
+    }
+}
+
+void delAll(simpul *root)
+{
+    if (root != NULL) // cek apakah root-nya kosong apa nggak
+    {
+        delAll(root->left);  // kalo ga kosong, bakal ngehapus mulai dari anak sebelah kiri
+        delAll(root->right); // lanjut ke anak sebelah kanan dihapus
+        free(root);          // hapus alokasi memori si root
+        // prosesnya rekursif dan terus-terusan mulai dari daun sampe akar
     }
 }
 
 void delLeft(simpul *root)
 {
-
-    if (root != NULL)
+    if (root != NULL) // cek apakah root-nya kosong apa nggak
     {
-        if (root->left != NULL)
+        if (root->left != NULL) // jika anak kiri dari root ga kosong
         {
-            delAll(root->left);
-            root->left = NULL;
+            delAll(root->left); // hapus anak sebelah kiri
+            root->left = NULL;  // null kan simpul anak kiri
         }
     }
 }
 
 void delRight(simpul *root)
 {
-
-    if (root != NULL)
+    if (root != NULL) // cek apakah root-nya kosong apa nggak
     {
-        if (root->right != NULL)
+        if (root->right != NULL) // jika anak kanan dari root ga kosong
         {
-            delAll(root->right);
-            root->right = NULL;
+            delAll(root->right); // hapus anak sebelah kanan
+            root->right = NULL;  // null kan simpul anak kanan
         }
-    }
-}
-
-void delAll(simpul *root)
-{
-
-    if (root != NULL)
-    {
-        delAll(root->left);
-        delAll(root->right);
-        free(root);
     }
 }
 
@@ -122,6 +119,8 @@ void copyTree(simpul *root1, simpul **root2)
     {
         (*root2) = (simpul *)malloc(sizeof(simpul));
         (*root2)->kontainer = root1->kontainer;
+        (*root2)->left = NULL;
+        (*root2)->right = NULL;
         if (root1->left != NULL)
         {
             copyTree(root1->left, &(*root2)->left);
@@ -138,8 +137,7 @@ int isEqual(simpul *root1, simpul *root2)
 
     int hasil = 1;
 
-    if ((root1 != NULL) &&
-        (root2 != NULL))
+    if ((root1 != NULL) && (root2 != NULL))
     {
         /* jika simpul pohon 1 dan pohon 2 sama sama
         tidak NULL */
@@ -153,16 +151,13 @@ int isEqual(simpul *root1, simpul *root2)
         {
             /* jika isi kontainer simpul pohon 1 dan simpul
             pohon 2 sama, maka telusuri simpul selanjutnya */
-            isEqual(root1->left,
-                    root2->left);
-            isEqual(root1->right,
-                    root2->right);
+            isEqual(root1->left, root2->left);
+            isEqual(root1->right, root2->right);
         }
     }
     else
     {
-        if ((root1 != NULL) ||
-            (root2 != NULL))
+        if ((root1 != NULL) || (root2 != NULL))
         {
             /* jika salah satu simpul ada yang NULL */
             hasil = 0;
--- Tes Mesin/TM8 - Binary Tree/header.h
+++ Tes Mesin/TM8 - Binary Tree/header.h
@@ -1,4 +1,12 @@
+/*
+Bismillah
+Saya Themy Sabri Syuhada dengan NIM 2203903
+berjanji mengerjakan tes mesin 8 dengan jujur
+demi keberkahan-Nya.
+Aamiin
+*/
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
 
 typedef struct smp *alamatsimpul;
@@ -13,3 +21,15 @@ typedef struct
 {
     simpul *root;
 } tree;
+
+void makeTree(char c, tree *T);
+void addLeft(char c, simpul *root);
+void addRight(char c, simpul *root);
+void delAll(simpul *root);
+void delLeft(simpul *root);
+void delRight(simpul *root);
+void printTreePreOrder(simpul *root);
+void printTreeInOrder(simpul *root);
+void printTreePostOrder(simpul *root);
+void copyTree(simpul *root1, simpul **root2);
+int isEqual(simpul *root1, simpul *root2);
